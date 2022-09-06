18dab3babbb7cc830efc82d924b2819f5cf31426
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-06 11:30:34 +0300

    Removed all store configurations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "../assets/styles.scss";
+// import { Provider, createStore } from "react-redux";
+// import store from "./store/index";
+// import createSto÷ scre from "./createReduxStore";
 
 const el = document.getElementById("app");
 
-ReactDOM.render(<App />, el);
+// const store = createStore();
+
+ReactDOM.render(
+ 
+    <App />,
+
+  el
+);
